92ebac10f3bc67cf0bc486d8391c2e186a033ba6
mazrommi <rokafordev@gmail.com>
2025-06-01 07:39:10 +0700

    nambah cors

--- src/application/web.js
+++ src/application/web.js
@@ -1,10 +1,12 @@
-import express from "express";
-import {publicRouter} from "../route/public-api.js";
-import {errorMiddleware} from "../middleware/error-middleware.js";
-import {userRouter} from "../route/api.js";
+import express from 'express';
+import { publicRouter } from '../route/public-api.js';
+import { errorMiddleware } from '../middleware/error-middleware.js';
+import { userRouter } from '../route/api.js';
+import cors from 'cors';
 
 export const web = express();
 web.use(express.json());
+web.use(cors());
 
 web.use(publicRouter);
 web.use(userRouter);
